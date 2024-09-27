b23e103e9c836b6f5f7887c650be5214aaa6f779
Janindu Chamod <janiduchamod2001425@gmail.com>
2024-09-27 15:19:31 +0530

    update server

--- User/backend/server.js
+++ User/backend/server.js
@@ -1,4 +1,5 @@
 import path from 'path';
+import cors from 'cors';
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -12,7 +13,7 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -20,18 +21,9 @@ app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-  const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, '/frontend/dist')));
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-  );
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
-}
+app.get('/', (req, res) => {
+  res.send('Server is ready');
+})
 
 app.use(notFound);
 app.use(errorHandler);
--- User/frontend/src/App.jsx
+++ User/frontend/src/App.jsx
@@ -3,14 +3,13 @@ import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/Header';
-import Navbar from './components/Navigation';
 
 const App = () => {
   return (
     <>
       <Header />
       <ToastContainer />
-      <Container className='my-2'>
+      <Container>
         <Outlet />
       </Container>
     </>
