d4188c4999a9a7c372e20c6ba178cd650ba7fe15
Janindu Chamod <janiduchamod2001425@gmail.com>
2024-09-30 22:17:07 +0530

    Create screens for genetic features

--- User/frontend/src/screens/Genetic_Engineering/Genetic_Trait.jsx
+++ User/frontend/src/screens/Genetic_Engineering/Genetic_Trait.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import Navigation from '../../components/Navigation'
+
+import Gene from '../../images/Genetic/gene.svg'
 
 const Genetic_Trait = () => {
   return (
-    <div>Genetic_Trait</div>
+    <div>
+      {/* Navigation Panel */}
+      <Navigation />
+
+      {/* Cover Image */}
+      <img src={Gene} className="cover_image" />
+
+      {/* Hero Section */}
+      <section className="genetic-hero">
+        <div className="hero-content">
+          <h1>
+            Unlock the Power of <br />
+            Genetic Engineering
+          </h1>
+          <p>
+            Empowering Farmers with Enhanced Crop Performance and Resilience
+          </p>
+          <button
+            className="cta-btn"
+            onClick={() =>
+              document
+                .getElementById("why")
+                .scrollIntoView({ behavior: "smooth" })
+            }
+          >
+            Explore More
+          </button>
+        </div>
+      </section>
+    </div>
   )
 }
 
